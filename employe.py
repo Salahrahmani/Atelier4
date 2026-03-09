084b2fa2042e87e60395f1d61f1504dbff7e8581
SALAH <salaheddine.rahmani@monborial.ca>
2026-03-08 22:09:45 -0400

    Ajouter La methodes Employe

--- employe.py
+++ employe.py
@@ -5,3 +5,34 @@ class Employe:
         self.nom = nom
         self.prenom = prenom
         self.voitureService = None
+
+    def afficherInformations(self):
+
+            print("Employe:", self.nom, self.prenom)
+
+            if self.voitureService:
+                print("Voiture:", self.voitureService.marque)
+            else:
+                print("Aucune voiture")
+    def affecterVoiture(self, voiture):
+
+        if self.voitureService:
+            print("Employe a deja une voiture")
+            return
+
+        if voiture.chauffeur:
+            print("Voiture deja attribuee")
+            return
+
+        self.voitureService = voiture
+        voiture.chauffeur = self
+    def retirerVoiture(self):
+
+        if self.voitureService is None:
+            print("Aucune voiture")
+            return
+
+        voiture = self.voitureService
+        self.voitureService = None
+        voiture.chauffeur = None
+    
