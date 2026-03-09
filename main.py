ff6a05f00d5a0554db82f4c773f7a905b4b40b59
SALAH <salaheddine.rahmani@monborial.ca>
2026-03-08 22:24:17 -0400

    Remove car

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ e2 = Employe("k198", "Saadi", "Ramzi")
 
 v1 = Voiture("HA579", 2025, "Renault", 19000)
 v2 = Voiture("OP372", 2026, "Kia", 4500)
-
+# affich info
 e1.afficherInformations()
 e2.afficherInformations()
 
 v1.afficherInformations()
 v2.afficherInformations()
-
+#give cars to the empl
 e1.affecterVoiture(v1)
 e2.affecterVoiture(v2)
 e1.afficherInformations()
 e2.afficherInformations()
+#remove car
+e1.retirerVoiture()
