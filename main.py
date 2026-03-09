8ddf85a4a940953e43f73e3bd50a2c472bc41569
SALAH <salaheddine.rahmani@monborial.ca>
2026-03-08 22:19:01 -0400

    creation d'objets employe + voiture

---
+++ main.py
@@ -0,0 +1,8 @@
+from employe import Employe
+from voiture import Voiture
+
+e1 = Employe("G687", "Rahmani", "Salah")
+e2 = Employe("k198", "Saadi", "Ramzi")
+
+v1 = Voiture("HA579", 2025, "Renault", 19000)
+v2 = Voiture("OP372", 2026, "Kia", 4500)
